494dcb200bb7770a7f4f61e59514979ee465d62f
npetersen <77npete@gmail.com>
2019-11-07 12:24:41 -0500

    changing directory structure

---
+++ fgobjectslib/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fgobjectslib',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Nick Petersen',
+    author_email='',
+    description=''
+)
