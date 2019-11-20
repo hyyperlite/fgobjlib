56c586b2381bc330750aea85fcf8ee8f908ea50f
npetersen <77npete@gmail.com>
2019-11-20 18:47:05 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='fgobjectslib',
+    version='',
+    packages=setuptools.find_packages(),
+    url='https://github.com/hyyperlite/fgobjlib',
+    license='',
+    author='Nick Petersen',
+    author_email='hyyperlite+github@gmail.com',
+    description='Many classes to represent Fortinet FortiGate objects and generate CLI or REST API configurations on-demand',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Os Independent"
+    ],
+    python_requires='>=3.6'
+)
